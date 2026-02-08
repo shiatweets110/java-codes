09fc8722ac689c71dc7eacf958ac29d001216f39
ali <naazhasan55@gmail.com>
2026-02-08 17:34:47 +0530

    initial

--- NewFile.java
+++ NewFile.java
@@ -2,5 +2,6 @@ public class NewFile {
     
         public static void main(String[] args) {
             System.out.println("This is a new file.");
+                System.out.println("This is a new file. by alimehdihasan");
         }
 }
